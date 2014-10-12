082876ceed90597d1566e070b6b8737ebbd15fba
hamster <sp576@cornell.edu>
2014-10-11 20:39:23 -0400

    changed package and testing psql

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	/*client.query('SELECT * FROM ', function(err, result) {
+		done();
+		if (err) {
+			console.error(err);
+		} else {
+			response.send(result.rows);
+		}
+  	});
+	*/
+  });
   response.send('Hello World!')
 })
 
